ea499aa86b6f860ebbf7d409910a1c9c05ea65f7
Bonnet de larbogne <bonnet_b@localhost.localdomain>
2013-05-13 07:38:08 +0200

    Removing commandModule directory, Do not need it anymore

--- commandModule/CommandCD.cpp
+++
@@ -1,8 +0,0 @@
-#include "UDF.hpp"
-#include <iostream>
-#include "CommandCD.hpp"
-
-bool CommandCD::execute(FileSystemTree &tree, UDF &udf) {
-  std::cout<<"Command cd executed."<<std::endl;
-  return true;
-}
--- commandModule/NodeInformation.cpp
+++
@@ -1,8 +0,0 @@
-#include "NodeInformation.hpp"
-
-NodeInformation::NodeInformation(bool isDirectory, std::string name, std::string timestamp, unsigned long size) {
-  this->isDirectory = isDirectory;
-  this->name = name;
-  this->timestamp = timestamp;
-  this->size = size;
-}
